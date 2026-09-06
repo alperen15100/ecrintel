3e08361014d78d938e15eefaa84b8e9ab957b4d8
alperen15100 <59732220+alperen15100@users.noreply.github.com>
2026-09-06 20:53:42 +0300

    Remove obsolete native map dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,5 +33,4 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("org.osmdroid:osmdroid-android:6.1.20")
 }
